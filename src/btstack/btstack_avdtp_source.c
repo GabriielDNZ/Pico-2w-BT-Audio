607e237134b1043bb5f65d55e7b3ebe969050093
GabriielDNZ <gabrieldiniz55@hotmail.com>
2026-06-28 14:20:16 -0300

    5

--- src/btstack/btstack_avdtp_source.c
+++ src/btstack/btstack_avdtp_source.c
@@ -84,7 +84,7 @@
 // period to keep the Bluetooth A2DP stream alive. Micro-gaps below this are not
 // filled with silence, which avoids the old picote/distorção.
 #define SBC_LOW_DELAY_FRAMES_PER_PACKET 1
-#define BT_KEEPALIVE_SILENCE_IDLE_MS 15
+#define BT_KEEPALIVE_SILENCE_IDLE_MS 5
 
 typedef struct {
     // bitmaps
@@ -226,7 +226,7 @@ static bool a2dp_is_connected_flag = false;
 
 static bool finish_scan_avdtp_codec = false;
 
-static uint8_t audio_timer_interval = 3;
+static uint8_t audio_timer_interval = 1;
 
 // on pico 2w the max stable aac bit rate under 512 simples without vbr is around 220000
 static uint8_t aac_audio_timer_interval = 12;
@@ -1567,7 +1567,7 @@ static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packe
                 sbc_configuration.max_bitpool_value,
                 sbc_configuration.channel_mode);
 
-            audio_timer_interval = 3;  // extreme 15 ms SBC pacing
+            audio_timer_interval = 1;  // ultra extreme 5 ms SBC pacing
 
             audio_slot_queue_configure_with_count(btstack_sbc_encoder_num_audio_frames(), AUDIO_SLOT_COUNT_SBC);
 
